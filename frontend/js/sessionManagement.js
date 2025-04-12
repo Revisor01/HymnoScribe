bcd57b967a3edad300d588dfb0cb7a7b8a395db6
Revisor01 <mail@simonluthe.de>
2025-04-12 22:27:02 +0200

    export

--- frontend/js/sessionManagement.js
+++ frontend/js/sessionManagement.js
@@ -74,7 +74,7 @@ export async function loadSession(id) {
     }
 }
 
-function loadConfigFromLocalStorage() {
+export function loadConfigFromLocalStorage() {
     try {
         const savedConfig = localStorage.getItem('liedblattConfig');
         if (savedConfig) {
